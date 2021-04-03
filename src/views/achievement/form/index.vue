d8995095015b34238a17543ab5342c91e7ce86d5
Ox915 <409981400@qq.com>
2021-04-03 17:25:36 +0800

    [feat]:change

--- src/views/achievement/form/index.vue
+++ src/views/achievement/form/index.vue
@@ -6,81 +6,53 @@
       </el-form-item>
       <el-row>
         <el-col :span="11">
-          <el-form-item label="成果介绍编号">
-            <el-input v-model="form.number" />
+          <el-form-item label="成果参与人员">
+            <el-input v-model="form.participantMember" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="2" class="line" />
+      </el-row>
+      <el-row>
+        <el-col :span="11">
+          <el-form-item label="获奖名称">
+            <el-input v-model="form.achieveName" />
           </el-form-item>
         </el-col>
         <el-col :span="2" class="line" />
       </el-row>
       <el-form-item label="创建时间">
         <el-col :span="11">
-          <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%;" />
+          <el-date-picker v-model="date1" type="date" placeholder="选择日期" style="width: 100%;" format="yyyy-MM-dd" value-format="yyyy-MM-dd" />
         </el-col>
         <el-col :span="2" class="line">-</el-col>
         <el-col :span="11">
-          <el-time-picker v-model="form.date2" type="fixed-time" placeholder="Pick a time" style="width: 100%;" />
+          <el-time-picker v-model="date2" type="fixed-time" placeholder="选择时间" style="width: 100%;" value-format="hh:mm:ss" />
         </el-col>
       </el-form-item>
       <el-form-item label="成果介绍类别">
-        <el-select v-model="form.value" placeholder="请选择">
+        <el-select v-model="value" placeholder="请选择">
           <el-option
-            v-for="item in form.options"
+            v-for="item in options.choice"
             :key="item.value"
             :label="item.label"
             :value="item.value"
+            @click="sortChoice(index)"
           />
         </el-select>
       </el-form-item>
-      <el-form-item prop="content" style="margin-bottom: 30px;" label="成果介绍概述">
-        <Tinymce ref="editor" :height="400" />
-      </el-form-item>
-      <el-form-item label="成果介绍图片">
-        <el-upload
-          action="#"
-          list-type="picture-card"
-          :auto-upload="false"
-          :class="{ hide: hideUp}"
-        >
-          <i slot="default" class="el-icon-plus" />
-          <div slot="file" slot-scope="{file}">
-            <img
-              class="el-upload-list__item-thumbnail"
-              :src="file.url"
-              alt=""
-            >
-            <span class="el-upload-list__item-actions">
-              <span
-                class="el-upload-list__item-preview"
-                @click="handlePictureCardPreview(file)"
-              >
-                <i class="el-icon-zoom-in" />
-              </span>
-              <span
-                v-if="!disabled"
-                class="el-upload-list__item-delete"
-                @click="handleDownload(file)"
-              >
-                <i class="el-icon-download" />
-              </span>
-              <span
-                v-if="!disabled"
-                class="el-upload-list__item-delete"
-                @click="handleRemove(file)"
-              >
-                <i class="el-icon-delete" />
-              </span>
-            </span>
-          </div>
-        </el-upload>
-        <el-dialog :visible.sync="dialogVisible">
-          <img width="100%" :src="dialogImageUrl" alt="">
-        </el-dialog>
-      </el-form-item>
-      <el-form-item style="margin-bottom: 30px;" label="成果介绍内容">
-        <Tinymce ref="editor" :height="400" />
+      <el-row>
+        <el-col :span="3">
+          <el-form-item label="热度">
+            <el-input v-model="form.hot" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="2" class="line" />
+      </el-row>
+      <el-form-item prop="content" style="margin-bottom: 30px;" label="成果介绍内容">
+        <Tinymce id="tinymceId" ref="editor" :height="400" :value="content" name="tinymac" />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">确定</el-button>
+        <el-button type="primary" @click="onSubmit,dataPost()">确定</el-button>
         <el-button @click="onCancel">取消</el-button>
       </el-form-item>
     </el-form>
@@ -93,13 +65,23 @@ export default {
   components: { Tinymce },
   data() {
     return {
+      id: 0,
+      operate: 0,
+      content: '',
       form: {
+        id: 0,
         title: '',
-        number: '',
-        date1: '',
-        date2: '',
-        author: '',
-        options: [{
+        content: '',
+        time: '',
+        sort: 0,
+        hot: 0,
+        participantMember: '',
+        achieveName: '',
+        img: ''
+      },
+      options: {
+        value: '',
+        choice: [{
           value: '选项1',
           label: '国内外赛事'
         }, {
@@ -111,24 +93,68 @@ export default {
         }, {
           value: '选项4',
           label: '应用实例'
-        }],
-        value: ''
+        }]
       },
-      dialogImageUrl: '',
-      dialogVisible: false,
-      disabled: false,
-      hideUp: true
+      type: ['国内外赛事', '科研成果', '涉及领域', '应用实例'],
+      value: '',
+      date1: '',
+      date2: '',
+      time: '',
+      tinymceId: ''
+    }
+  },
+  created() {
+    this.id = this.$route.query.id
+    this.operate = this.$route.query.operate
+    const that = this
+    if (this.operate === 2) {
+      this.$axios
+        .get('http://localhost:8083/achieve/giveID?id=' + this.id)
+        .then(function(response) {
+          that.form = response.data
+          that.value = that.type[that.form.sort]
+          that.time = that.form.time
+          that.date1 = that.time.substr(0, 10)
+          that.date2 = that.time.substr(11, 8)
+          that.content = that.form.content
+        })
     }
   },
   methods: {
     onSubmit() {
       this.$message('submit!')
     },
+    sortChoice(index) {
+      this.form.sort = index
+    },
     onCancel() {
       this.$message({
         message: 'cancel!',
         type: 'warning'
       })
+      this.$router.go(-1)
+    },
+    getValue(value) {
+      switch (value) {
+        case '选项1':
+          this.form.sort = 0
+          break
+        case '选项2':
+          this.form.sort = 1
+          break
+        case '选项3':
+          this.form.sort = 2
+          break
+        case '选项4':
+          this.form.sort = 3
+          break
+        default :
+          this.form.sort = 0
+          break
+      }
+    },
+    getContent() {
+      this.form.content = window.tinymce.get('tinymceId').getContent()
     },
     handleRemove(file) {
       console.log(file)
@@ -139,6 +165,48 @@ export default {
     },
     handleDownload(file) {
       console.log(file)
+    },
+    dataPost() {
+      this.$confirm('确认是否编辑完毕?', '提示', {
+        confirmButtonText: '提交',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        var that = this
+        this.listLoading = true
+        if (this.operate === 2) {
+          this.form.time = this.date1 + 'T' + this.date2
+          this.getValue(this.value)
+          this.getContent()
+          this.$axios
+            .post('http://localhost:8083/achieve/updataID', this.form)
+            .then(function(response) {
+              that.listLoading = false
+            })
+          this.$message({
+            type: 'success',
+            message: '修改成功!'
+          })
+        } else {
+          this.form.time = this.date1 + 'T' + this.date2
+          this.getValue(this.value)
+          this.getContent()
+          this.$axios
+            .post('http://localhost:8083/achieve/insertID', this.form)
+            .then(function(response) {
+              that.listLoading = false
+            })
+          this.$message({
+            type: 'success',
+            message: '添加成功!'
+          })
+        }
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消添加'
+        })
+      })
     }
   }
 }
